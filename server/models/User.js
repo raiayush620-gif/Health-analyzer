a9999c5cff1c2a7616689f2af8f8b565a914ba91
Ayush <raiayush620@gmail.com>
2026-06-29 23:25:13 +0530

    Fix password re-hashing bug in User model

--- server/models/User.js
+++ server/models/User.js
@@ -45,11 +45,12 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 // Encrypt password using bcrypt before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
